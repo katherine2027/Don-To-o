3958ae43a3c6f027d34ee6a01dfe61f28819721d
katherine2027 <katherine.esquivel2027@adoc.superate.org>
2026-04-27 11:04:20 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+   $host ="localhost";
+   $user = "root";
+   $password = "";
+   $dbname = "dontono_db";
+   $conn = new mysqli($host, $user, $password, $dbname);
+
+   if($conn->connect_error) {
+    die("". $conn->connect_error);
+   }
+
+   $conn->set_charset("utf8mb4");
+?>
