05fcb6bd2fd5a31c977dfe0e94d90e3b1ff22607
katherine2027 <katherine.esquivel2027@adoc.superate.org>
2026-04-29 00:05:43 -0600

    index.php

--- index.php
+++ index.php
@@ -28,8 +28,8 @@
                     Productos
                     </a>
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Herrramientas</a></li>
-                    <li><a class="dropdown-item" href="#">Construccion</a></li>
+                    <li><a class="dropdown-item" href="#">Herramientas</a></li>
+                    <li><a class="dropdown-item" href="#">Construcción</a></li>
                     <li><a class="dropdown-item" href="#">Techo</a></li>
                 </ul>
                 </li>
@@ -50,39 +50,49 @@
     <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="2" aria-label="Slide 3"></button>
     <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  <div id="carouselExampleFade" class="carousel slide carousel-fade">
+   <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="img/OIP (1) (1).png" class="d-block w-100" alt="...">
-           <div class="carousel-caption d-none d-md-block">
-             <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="img/OIP.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-             <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="img/OIP (1) (1).png" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/OIP (1) (1).png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
     </div>
+
+    <div class="carousel-item">
+      <img src="img/OIP.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+
+    <div class="carousel-item">
+      <img src="img/OIP (1) (1).png" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5 mx-5">
    <div class="col-10 col-sm-8 col-lg-6"> 
