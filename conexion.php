719f2ca1edc33a0fa05b96d6abb3df542b636e9a
katherine2027 <katherine.esquivel2027@adoc.superate.org>
2026-04-28 23:33:27 -0600

    conexion.php

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@
    $conn = new mysqli($host, $user, $password, $dbname);
 
    if($conn->connect_error) {
-    die("". $conn->connect_error);
+    die("Error de conexión:". $conn->connect_error);
    }
 
    $conn->set_charset("utf8mb4");
